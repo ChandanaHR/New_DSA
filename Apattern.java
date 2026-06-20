909279b3908ce85bbcb809ce8bb5758ff4e506d0
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-20 18:42:57 +0530

    Update Apattern.java

--- Apattern.java
+++ Apattern.java
@@ -35,3 +35,41 @@ public class Main
 	  }
 	}
 }
+
+//For even numbers
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+	  int n = sc.nextInt();
+	  int row = n;
+	  int col = n+(n-1);
+	  int newval = n-1;
+	  int mid = n/2-1;
+	  for(int i=0;i<row;i++) {
+	      String str = "";
+	      for(int j=0;j<col;j++) {
+	          if(j<=newval && (j+i)==newval && i!=mid){
+	              str = str + "*";
+	          }
+	          else if(j>newval && (j-i)==newval && i!=mid) {
+	              str = str + "*";
+	          }
+	          else if(j<=newval && (j+i)>=newval && i==mid) {
+	              str = str + "*";
+	          }
+	          else if(j>newval && (j-i)<=newval && i==mid) {
+	              str = str + "*";
+	          }
+	          else {
+	              str = str + " ";
+	          }
+ 	      }
+ 	      System.out.println(str);
+	  }
+	}
+}
