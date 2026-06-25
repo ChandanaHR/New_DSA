252432687aabf7acccb6715ec9f246d32a199727
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-25 20:44:36 +0530

    Create Epattern.java

---
+++ Epattern.java
@@ -0,0 +1,44 @@
+//For odd numbers
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int mid = n/2;
+		for(int i=0;i<n;i++) {
+		    String str = "";
+		    for(int j=0;j<n;j++) {
+		        if((i==0||i==mid||i==n-1) || (j==0)) {
+		            str = str+"*";
+		        }
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+//For even numbers
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int mid = n/2;
+		for(int i=0;i<n;i++) {
+		    String str = "";
+		    for(int j=0;j<n;j++) {
+		        if((i==0||i==mid-1||i==n-1) || (j==0)) {
+		            str = str+"*";
+		        }
+		    }
+		    System.out.println(str);
+		}
+	}
+}
