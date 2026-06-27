b3bac4eb78488601357882e7b6b6154778a7f4a1
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-27 18:50:52 +0530

    Create GPattern.java

---
+++ GPattern.java
@@ -0,0 +1,34 @@
+//For odd and well as even numbers
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n = sc.nextInt();
+	   int mid = n/2;
+	   for(int i=0;i<n;i++) {
+	       String str = "";
+	       for(int j=0;j<n;j++) {
+	           if((i==0||i==n-1) && (j!=0)){
+	               str = str + "*";
+	           }
+	           else if((i!=0&&i!=n-1) && (j==0)) {
+	               str = str + "*";
+	           }
+	           else if(i==mid && j>=mid) {
+	               str = str + "*";
+	           }
+	           else if((i>mid && i<n-1) && j==n-1) {
+	               str = str + "*";
+	           }
+	           else {
+	               str = str + " ";
+	           }
+	       }
+	       System.out.println(str);
+	   }
+	}
+}
