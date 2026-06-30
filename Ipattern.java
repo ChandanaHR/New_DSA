23b251a9f4a64d5adbda709cb75179447d69f317
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-30 16:15:34 +0530

    Create Ipattern.java

---
+++ Ipattern.java
@@ -0,0 +1,46 @@
+//even
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int mid = n/2-1;
+		for(int i=0;i<n;i++){
+		    String str = "";
+		    for(int j=0;j<n;j++){
+		        if((i==0 || i==n-1) || (j==mid))
+		            str = str + "*";
+		        else 
+		            str = str + " ";
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+//odd
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int mid = n/2;
+		for(int i=0;i<n;i++){
+		    String str = "";
+		    for(int j=0;j<n;j++){
+		        if((i==0 || i==n-1) || (j==mid))
+		            str = str + "*";
+		        else 
+		            str = str + " ";
+		    }
+		    System.out.println(str);
+		}
+	}
+}
